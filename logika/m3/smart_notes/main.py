b6f8dfd5e9f1806dc13b59b6927c8f87490b4d08
KInobraC <140088037+KInobraC@users.noreply.github.com>
2023-10-10 19:16:11 +0300

    Add files via upload

--- logika/m3/smart_notes/main.py
+++ logika/m3/smart_notes/main.py
@@ -20,33 +20,53 @@ lst_notes = QListWidget()
 btn_note_create = QPushButton("Стровити замітку")
 btn_note_del = QPushButton("Видалити замітку")
 btn_note_save = QPushButton("Зберегти замітку")
+
 lb_tags = QLabel("Список тегів")
+
+
 lst_tags = QListWidget()
+
 field_tag = QLineEdit()
 btn_tags_add = QPushButton("Додати до замітки")
 btn_tgs_del = QPushButton("Відкріпити від заміти")
 btn_tags_search = QPushButton("Шукати замітки за тегом")
+
+
 row1 = QHBoxLayout()
+
 row1.addWidget(btn_note_create)
 row1.addWidget(btn_note_del)
+
 row2 = QHBoxLayout()
+
+
 layout_notes = QHBoxLayout()
+
 col1 = QVBoxLayout()
 col2 = QVBoxLayout()
+
 col2.addWidget(lb_notes)
 col2.addWidget(lst_notes)
+
 layout_notes.addLayout(col1, stretch=2)
 layout_notes.addLayout(col2, stretch=1)
+
 row2.addWidget(btn_tags_search)
 row2.addWidget(btn_tgs_del)
+
 col2.addWidget(btn_note_save)
+
+
 col1.addWidget(field_text)
+
 col2.addLayout(row1)
 col2.addWidget(lb_tags)
 col2.addWidget(lst_tags)
 col2.addWidget(field_tag)
+
 col2.addWidget(btn_tags_add)
 col2.addLayout(row2)
+
 def show_notes():
     key = lst_notes.selectedItems()[0].text()
     field_text.setText(notes[key]['текст'])
@@ -79,8 +99,56 @@ def save_note():
 
         notes[key]["текст"] = field_text.toPlainText()
         writeToFile()
+def add_noteses():
+    key = lst_notes.currentItem().text()
+    tag = field_tag.text()
+    notes[key]['теги'].append(tag)
 
-btn_note_save.clicked.connect(save_note)
+    lst_tags.addItem(tag)
+
+    writeToFile()
+def del_noteses():
+    key = lst_notes.currentItem().text()
+    tag = lst_tags.currentItem().text()
+
+    notes[key]['теги'].remove(tag)
+    lst_tags.clear()
+    lst_tags.addItems(notes[key]['теги'])
+
+    writeToFile()
+def search_note():
+    tag  = field_tag.text()
+
+    if "Шукати замітки за тегом" == btn_tags_search.text():
+        filtered_notes = {}
+
+        for key in notes:
+            if tag in notes[key]["теги"]:
+                filtered_notes[key] = notes[key]
+
+        btn_tags_search.setText("Скинути пошук")
+
+        lst_notes.clear()
+        lst_notes.addItems(filtered_notes)
+        lst_tags.clear()
+
+        field_text.clear()
+
+
+    elif "Скинути пошук" == btn_tags_search.text():
+        btn_tags_search.setText("Шукати за тегом")
+
+        lst_notes.clear()
+        lst_notes.addItems(notes)
+        lst_tags.clear()
+
+        field_text.clear()
+        field_tag.clear()
+
+
+btn_tags_add.clicked.connect(add_noteses)
+btn_tgs_del.clicked.connect(del_noteses)
+btn_tags_search.clicked.connect(search_note)
 
 btn_note_del.clicked.connect(del_note)
 
@@ -92,6 +160,12 @@ lst_notes.itemClicked.connect(show_notes)
 with open("notes.json", "r", encoding="utf8") as file:
     notes = json.load(file)
 lst_notes.addItems(notes)
+
+
+
+
+
+
 window.setLayout(layout_notes)
 window.show()
 app.exec_()
